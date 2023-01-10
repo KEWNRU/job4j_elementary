b1cef12d385860d5fb6b2ad7453551a4baf92bf4
KEWNRU <n0kky21rus@gmail.com>
2023-01-10 11:29:50 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Ivan Popov");
+        System.out.println("06.06.1997");
+
+    }
+}
