274bae741101e2dbe2d66c32c79dd92d88fd8f07
KEWNRU <n0kky21rus@gmail.com>
2023-01-12 14:49:53 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(double height) {
+        double rsl = height / 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(double height) {
+        double rsl = height / 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 100;
+        short height1 = 110;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height1);
+        System.out.println("Man 100 is " + man);
+        System.out.println("Woman 110 is " + woman);
+    }
+}
