857fc729c40654f62716ea533df33991f59b6e3f
KEWNRU <n0kky21rus@gmail.com>
2023-01-20 20:18:35 +0300

    1.1. Что такое import [#504773 #382977]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -2,6 +2,7 @@ package ru.job4j.calculator;
 
 import static ru.job4j.math.MathFunction.*;
 public class MathCalculator {
+
     public static double sumAndMinus(double first, double second) {
         return sum(first, second)
                 + minus(first, second);
@@ -18,7 +19,7 @@ public class MathCalculator {
                 + share(first, second)
                 + minus(first, second);
     }
-    
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMinus(25, 43));
         System.out.println("Результат расчета равен: " + shareAndMinus(11, 32));
