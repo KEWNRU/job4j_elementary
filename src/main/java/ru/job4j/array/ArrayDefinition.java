f637e2450c9537ffc791f527c6d084835b0a2a32
KEWNRU <n0kky21rus@gmail.com>
2023-01-31 12:26:09 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,14 +1,18 @@
 package ru.job4j.array;
 
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Arrays;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива равен: " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Popov";
+        names[1] = "Natahsa Popova";
+        names[2] = "Vav4ik Popov";
+        names[3] = "Petr Arsentev";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
