5e8cd7a336774952a7c279969e5075057a774a59
KEWNRU <n0kky21rus@gmail.com>
2023-02-04 11:55:41 +0300

    6.4.3. Слово заканчивается на ... [#156317 #388107]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,6 +2,13 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        return word[word.length - 1] == post[post.length - 1];
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (post[post.length - 1 - i] != word[word.length - i - 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
