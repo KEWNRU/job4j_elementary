cc4eb66ad1fe0afcf344e1f2ceaa0b717c265847
KEWNRU <n0kky21rus@gmail.com>
2023-02-05 17:14:47 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #388552]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-        for (int i = 0; i < el; i++) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 rst = i;
                 break;
@@ -14,7 +14,7 @@ public class FindLoop {
 
     public static int indexInRange(int[] data, int el, int start, int finish) {
         int rst = -1;
-        for (int i = start; i <= data[finish]; i++) {
+        for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
                 break;
