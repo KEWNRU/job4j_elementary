834de609cac3a28f43d9d6ffd3d8650e2c599acc
KEWNRU <n0kky21rus@gmail.com>
2023-02-09 18:51:47 +0300

    6.9. String - это массив [#504884 #390056]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -4,19 +4,20 @@ import static java.lang.Character.*;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        if (name.isEmpty() || !isLowerLatinLetter(name.codePointAt(0))) {
-            return false;
-        }
+        boolean rsl = false;
+        if (!name.isEmpty()) {
             for (int i = 0; i < name.length(); i++) {
                 int code = name.codePointAt(i);
                 if (code < 65 || code > 90) {
-                    if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || isDigit(code)) {
-                        return true;
+                    if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || !isDigit(code)) {
+                        rsl = true;
                     }
+                } else {
                     break;
                 }
             }
-        return false;
+        }
+        return rsl;
     }
 
     public static boolean isSpecialSymbol(int code) {
